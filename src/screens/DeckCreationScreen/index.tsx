d8a72d61a350712224fa3dd5cf6581f7c637a05e
Vladimir Radovanovich <vlad.radovanovich@gmail.com>
2026-01-31 04:17:04 +0100

    minor change

--- src/screens/DeckCreationScreen/index.tsx
+++ src/screens/DeckCreationScreen/index.tsx
@@ -1,18 +1,12 @@
 import { FlipCardWrapper } from "@/shared/components/FlipCardWrapper";
 import st from "./styles.module.scss";
-import { useState } from "react";
 
 export function DeckCreationScreen() {
-	const [cardIsFlipped, setCardIsFlipped] = useState(false);
-
 	return (
 		<div className={st.container}>
-			<FlipCardWrapper isOnBack={cardIsFlipped} />
-
-			<button onClick={() => setCardIsFlipped(!cardIsFlipped)}>
-				Flip
-				{/* <CardFlipIcon /> */}
-			</button>
+			<div className={st.content}>
+				<FlipCardWrapper />
+			</div>
 		</div>
 	);
 }
--- src/shared/components/FlipCardWrapper/index.tsx
+++ src/shared/components/FlipCardWrapper/index.tsx
@@ -1,11 +1,9 @@
-import { CSSProperties } from "react";
+import { CSSProperties, useState } from "react";
 import st from "./styles.module.scss";
 
-interface I_FlipCardWrapperProps {
-	isOnBack: boolean;
-}
+export function FlipCardWrapper() {
+	const [isOnBack, setIsOnBack] = useState(false);
 
-export function FlipCardWrapper({ isOnBack }: I_FlipCardWrapperProps) {
 	return (
 		<div
 			className={st.container}
@@ -15,8 +13,24 @@ export function FlipCardWrapper({ isOnBack }: I_FlipCardWrapperProps) {
 				} as CSSProperties
 			}>
 			<div className={st.content}>
-				<div className={st.front}> front of the card</div>
-				<div className={st.back}>back of the card</div>
+				<div className={st.top}>
+					<button onClick={() => setIsOnBack(!isOnBack)}>
+						Flip
+						{/* <CardFlipIcon /> */}
+					</button>
+				</div>
+				<div className={st.middle}>
+					<div className={st.front}>
+						<h1>question</h1>
+					</div>
+					<div className={st.back}>
+						<h1>answer</h1>
+					</div>
+				</div>
+				<div className={st.bottom}>
+					chat UI goes here with media uploads and eventually
+					camera access on mobile
+				</div>
 			</div>
 		</div>
 	);
